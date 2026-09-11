2cfc1325db2d29c1540a684256e1e83850ce5c6e
asim1417 <aasemalfarsi@gmail.com>
2026-09-11 21:21:13 +0300

    feat(dd): surface government open-data hub in dashboard

--- app/dashboard/due-diligence/page.tsx
+++ app/dashboard/due-diligence/page.tsx
@@ -1,6 +1,7 @@
 import { DueDiligenceHistory } from "@/components/due-diligence/DueDiligenceHistory";
 import { DueDiligenceSourceMatrix } from "@/components/due-diligence/DueDiligenceSourceMatrix";
 import { DueDiligenceWorkbench } from "@/components/due-diligence/DueDiligenceWorkbench";
+import { GovernmentOpenDataHub } from "@/components/due-diligence/GovernmentOpenDataHub";
 
 export const dynamic = "force-dynamic";
 
@@ -8,6 +9,7 @@ export default function DueDiligencePage() {
   return (
     <div className="wb-page">
       <DueDiligenceWorkbench />
+      <GovernmentOpenDataHub />
       <DueDiligenceSourceMatrix />
       <DueDiligenceHistory />
     </div>
