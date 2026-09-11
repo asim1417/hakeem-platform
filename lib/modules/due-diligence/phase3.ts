73aa61f9d62d6de9ddec58f1417bf2c6cec98888
asim1417 <aasemalfarsi@gmail.com>
2026-09-11 14:20:25 +0300

    feat(dd): compose SAMA and CMA regulator sources

--- lib/modules/due-diligence/phase3.ts
+++ lib/modules/due-diligence/phase3.ts
@@ -3,10 +3,18 @@ import {
   CMA_CAPITAL_MARKET_INSTITUTIONS_SOURCE,
   CmaCapitalMarketInstitutionsConnector,
 } from "./cma";
+import {
+  SAMA_FINANCE_ENTITIES_SOURCE,
+  SamaFinanceEntitiesConnector,
+} from "./sama";
 import { buildPhase2Connectors, phase2SourceCatalog } from "./phase2";
 
 export function buildPhase3Connectors(): DueDiligenceConnector[] {
-  return [...buildPhase2Connectors(), new CmaCapitalMarketInstitutionsConnector()];
+  return [
+    ...buildPhase2Connectors(),
+    new CmaCapitalMarketInstitutionsConnector(),
+    new SamaFinanceEntitiesConnector(),
+  ];
 }
 
 export function phase3SourceCatalog(): DataSourceDefinition[] {
@@ -16,6 +24,7 @@ export function phase3SourceCatalog(): DataSourceDefinition[] {
   return [
     ...concrete,
     { ...CMA_CAPITAL_MARKET_INSTITUTIONS_SOURCE },
+    { ...SAMA_FINANCE_ENTITIES_SOURCE },
     ...(genericRegulator ? [genericRegulator] : []),
   ];
 }
